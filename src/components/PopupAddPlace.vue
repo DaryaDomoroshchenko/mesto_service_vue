3d7af1be5199127a657b162256dcb39b6a9e333e
Darya Domoroshchenko <deydra.v@gmail.com>
2020-07-02 22:49:40 +0400

    Реализована возможность добавлять и удалять фотографии, ставить лайки

---
+++ src/components/PopupAddPlace.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="popup popup__addPlace">
+    <div class="popup__content">
+      <img @click="hide" src="@/assets/images/close.svg" alt="Закрыть" class="popup__close"/>
+      <h4 class="popup__title">Новое место</h4>
+      <form @submit.prevent="addCard" class="popup__form" name="add">
+        <input
+          v-model="name"
+          type="text"
+          name="name"
+          class="popup__input popup__input_type_name"
+          placeholder="Название"
+        />
+        <input
+          v-model="link"
+          type="text"
+          name="link"
+          class="popup__input popup__input_type_link-url"
+          placeholder="Ссылка на картинку"
+        />
+        <button type="submit" class="button popup__button popup__button_add">+</button>
+      </form>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "PopupAddPlace",
+
+  data() {
+    return {
+      name: "",
+      link: ""
+    };
+  },
+
+  methods: {
+    hide() {
+      this.$emit("hide-popup");
+    },
+
+    addCard() {
+      this.hide();
+      const cardData = {
+        name: this.name,
+        link: this.link
+      };
+
+      this.$emit("add-card", cardData);
+    }
+  }
+};
+</script>
